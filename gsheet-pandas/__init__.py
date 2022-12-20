12ca62f083d7e98c93cfda10dd2cd342f9d11258
Iakov <yashahelfi2012@gmail.com>
2022-12-20 15:35:39 +0300

    Fixed setup.py

--- gsheet-pandas/__init__.py
+++ gsheet-pandas/__init__.py
@@ -1,4 +1,4 @@
-from connection import DriveConnection
+from gsheet_pandas.connection import DriveConnection
 
 
 __version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup
 
 setup(
     name='gsheet-pandas',
-    version='0.1.1',
+    version='0.1.2',
     description='Download and upload pandas dataframes to the Google sheets',
     url='https://github.com/iakov-kaiumov/gsheet-pandas',
     author='Iakov Kaiumov',
-    author_email='shudson@anl.gov',
+    author_email='kaiumov.iag@phystech.edu',
     license='MIT License',
-    packages=['gsheet-pandas'],
+    packages=['gsheet_pandas'],
+    package_dir={'gsheet_pandas': 'gsheet-pandas'},
     install_requires=[
         'google-api-python-client',
         'google-api-core',
